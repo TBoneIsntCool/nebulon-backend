e3c79a63a08cf42c55a630993ef0ea52040b4d4b
TBone <118640887+TBoneIsntCool@users.noreply.github.com>
2025-03-15 10:41:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ addEventListener('fetch', event => {
 })
 
 async function handleRequest(request) {
-  const apiUrl = 'https://astroservercheck.joejoetv.de/api/check?url=147.185.221.16:23237'; // Your API URL
-  try {
-    const apiResponse = await fetch(apiUrl);
-    if (!apiResponse.ok) {
-      return new Response('Error fetching data from the API', { status: 500 });
-    }
-    const data = await apiResponse.json();
-    return new Response(JSON.stringify(data), {
-      headers: { 'Content-Type': 'application/json' }
-    });
-  } catch (error) {
-    return new Response('Error with the worker request', { status: 500 });
+  const response = await fetch('https://nebulon-backend.tbone-unity.workers.dev');
+  const data = await response.json();
+
+  // Add CORS headers to allow cross-origin requests
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type',
   }
+
+  return new Response(JSON.stringify(data), {
+    headers: headers
+  });
 }
